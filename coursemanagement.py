746c933e2990b8452c2c60db7a4bce3c67c0c23d
jchengroa <chengroaj2@gmail.com>
2025-03-07 10:36:17 +0800

    Version 0.1
    Initial File
    > added loadsave and createsave functions

---
+++ coursemanagement.py
@@ -0,0 +1,25 @@
+"""
+<COURSEMANAGEMENT FILE DOCSTRING>
+Name: John Carlo E. Cheng Roa
+ID Number: 12447935
+
+Version: 0.1
+> added loadsave and createsave functions
+"""
+
+# Import System Files
+import os
+
+# Functions
+
+def loadsave():
+    os.system('cls')
+    print("Load Save")
+    input("<< < Enter Any Character to Exit this Page > >>\n")
+    pass
+
+def createsave():
+    os.system('cls')
+    print("Create Save")
+    input("<< < Enter Any Character to Exit this Page > >>\n")
+    pass
